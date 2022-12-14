6a854a36ca358b00a6d54bbcf23aa5f2e41a4652
HelianthusAnuus13 <duy9117@gmail.com>
2022-12-14 23:58:48 +0700

    Làm phần Header và responsive cho menu navbar

---
+++ src/components/home/Data.jsx
@@ -0,0 +1,7 @@
+const Data = () => {
+    return ( 
+        <h1>Data</h1>
+    );
+}
+
+export default Data;
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+function Social() {
+    return (
+        <div className="home__social">
+            <a href="https://www.facebook.com/helianthusanuus1003" className="home__social-icon" target="_blank">
+                <i class="uil uil-facebook"></i>
+            </a>
+            <a href="https://www.google.com/intl/vi/gmail/about/" className="home__social-icon" target="_blank">
+                <i class="uil uil-envelope-alt"></i>
+            </a>
+            <a href="https://github.com/HelianthusAnnuus1003" className="home__social-icon" target="_blank">
+                <i class="uil uil-github"></i>
+            </a>
+        </div>
+    );
+}
+
+export default Social;
